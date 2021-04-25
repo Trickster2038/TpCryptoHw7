abc0271d12c643e20ae19831145ee9e1437b34a6
Sergey <33185046+Trickster2038@users.noreply.github.com>
2021-04-25 16:33:46 +0300

    proto 3

--- main.py
+++ main.py
@@ -2,37 +2,68 @@ import sys
 from Crypto.Protocol.SecretSharing import Shamir
 import secretgen
 from Crypto.Random import get_random_bytes
+import binascii
 # int('0x11',16)
 # print(sys.argv[1])
 
 
 if __name__ == '__main__':
+    print("=== WELCOME TO SHAMIR SPLITTER ===")
+    arg = sys.argv[1]
 
-	print("=== WELCOME TO SHAMIR SPLITTER ===")
+    if(arg == 'generate'):
+        print('\nGenerated key:')
+        print(secretgen.generate())
 
-	arg = sys.argv[1]
+    elif(arg == 'split'):
+        print('\nInput secret:')
+        secret_str = input()
+        # secret_str = "0x3454078a54a22d0c97db7f294b376278"
 
-	if(arg == 'generate'):
-		print('\nGenerated key:')
-		print(secretgen.generate())
-	elif(arg == 'split'):
-		print('\nInput secret:')
+        #secret_int = int(secret_str, 16)
+        secret_bytes = bytes.fromhex(secret_str[2:])
 
-		secret_str = input()
-		# secret_str = "0x3454078a54a22d0c97db7f294b376278"
-		secret_int = int(secret_str, 16)
-		secret_bytes = bytes.fromhex(secret_str[2:])
-		print('\nInput N T (N >= T):')
+        print('\nInput N T (N >= T):')
+        nt_strs = input().split()
+        #nt_strs = "10 3"
+        n = int(nt_strs[0])
+        t = int(nt_strs[1])
 
-		nt_strs = input().split()
-		#nt_strs = "10 3"
+        # exam = get_random_bytes(16)
+        shares = Shamir.split(t, n, secret_bytes)
 
-		n = int(nt_strs[0])
-		t = int(nt_strs[1])
+        print('\nSplitted secret:')
+        for idx, share in shares:
+            print("Part {}: 0x{}".format(idx, share.hex()))
 
-		# exam = get_random_bytes(16)
-		shares = Shamir.split(t, n, secret_bytes)
+    elif(arg == 'recover'):
+        shares = [[1, "2bae6ed5ba407223348226a4e1e58a42"],[2, "fa7b3591a728f18d870c2939bc3a2f91"]]
+        # shares = []
+        # fl = True
+        # print('\nInput idx and share separated by ":" (or press enter to exit):')
+        # while fl:
+        #     part_str = input()
+        #     if len(part_str) != 0:
+        #         idx, share = [ s.strip() for s in part_str.split(":") ]
+        #         #part_int = int(share, 16)
+        #         inner_share = share[2:]
+        #         part_bytes = binascii.unhexlify(inner_share)
+        #         shares.append((idx, part_bytes))
+        #     else:
+        #         fl = False
 
-		print('\nSplitted secret:')
-		for idx, share in shares:
-			print("Part {}: {}".format(idx, share.hex()))
+        #shares = [[1, "2bae6ed5ba407223348226a4e1e58a42"],[2, "fa7b3591a728f18d870c2939bc3a2f91"]]
+
+        bin_shares = [] 
+
+        for share in shares:
+            bin_shares.append((share[0], binascii.unhexlify(share[1])))
+
+        for idx, share in shares:
+            print("Part {}: 0x{}".format(idx, share))
+
+        for idx, share in bin_shares:
+            print("Part {}: 0x{}".format(idx, share))
+
+        key = Shamir.combine(bin_shares)
+        print('0x{}'.format(key.hex()))
